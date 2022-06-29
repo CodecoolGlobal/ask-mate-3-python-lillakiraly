443ee7271b2a9aa9ec1552d3742d6575a2b388a2
Maarsupilami <vajger.mark@outlook.hu>
2022-06-29 14:21:47 +0200

    ADD image handling foos

--- util.py
+++ util.py
@@ -1,52 +1,29 @@
+import os
 from datetime import datetime
-from time import mktime
+
+from werkzeug.utils import secure_filename
+
+from SETTINGS import ALLOWED_EXTENSIONS, PATH, SUBMISSION_TIME
 
 
-def increase_page_view_num(question_id):
-    data = data_handler.read_csv(reverse=False)
-    for row in data:
-        if int(row['id']) == int(question_id):
-            row['view_number'] = int(row['view_number']) + 1
-    data_handler.write_questions_to_csv(data)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def convert_date(datas=None, time_data=None):
-    # for question in datas:
-    #     for key in question:
-    #         if key == sorted_by:
-    #             question[key] = int(question[key])
-    # return sorted(datas, key=operator.itemgetter(sorted_by), reverse=(order == 'desc'))
-    if time_data is not None:
-        position_of_last_dot = str(time_data).rfind(".")
-        return int(mktime(datetime.strptime(str(time_data)[:position_of_last_dot], '%Y-%m-%d %H:%M:%S').timetuple()))
-
-    for data in datas:
-        for key in data:
-            if key == 'submission_time':
-                data[key] = datetime.utcfromtimestamp(int(data[key])).strftime('%Y-%m-%d %H:%M:%S')
-    return datas
+def get_timestamp():
+    return str(datetime.now().timestamp())
 
 
-def generate_id(data):
-    ids = [int(item['id']) for item in data]
-    if not ids:
-        return 1
-    return max(ids) + 1
+def upload_image(file):  # sourcery skip: replace-interpolation-with-fstring, use-fstring-for-formatting
+    filename = secure_filename(file.filename)
+    image = '{}-{}'.format(int(datetime.now().timestamp()), filename)
+    file.save(os.path.join(PATH, image))
+    image = 'images/%s' % image
+    return image
 
 
-def vote(id: int, csvdata: str, voting_direction: str, switch=True):
-    all_data = data_handler.read_csv(csv_database=csvdata, reverse=False)
-    for row in all_data:
-        if int(row['id']) == int(id):
-            row['id'] = id
-            if voting_direction == 'up':
-                row['vote_number'] = int(row['vote_number']) + 1
-            else:
-                row['vote_number'] = int(row['vote_number']) - 1
-    data_handler.write_questions_to_csv(export=all_data, switch=switch)
-
-
-if __name__ == '__main__':
-    print(convert_date(datetime.now(), to_timestamp=True))
-    print(convert_date(1655151674, to_timestamp=False))
-    print(generate_id(data=data_handler.read_csv()))
+def modify_request_form(dict_, filename):
+    dict_['submission_time'] = SUBMISSION_TIME
+    dict_['image'] = filename
+    return dict_
